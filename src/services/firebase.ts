857212ee26fa3b3f4aa93669dec15fa132e17f7d
Rodrigo Dulizio <rodrigo.dulizio@gmail.com>
2024-01-27 12:22:22 -0300

    class 02: at 45min

---
+++ src/services/firebase.ts
@@ -0,0 +1,21 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_DATABASE_URL,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Acesse diretamente os submódulos
+export const auth = firebase.auth();
+export const database = firebase.database();
